1e075944febfadeb56f8de44509ecee2c9c38191
pkey <kutka.paulius@gmail.com>
2019-12-12 00:38:30 +0100

    Fixed update issue

--- packages/server/src/services/ticket.service.ts
+++ packages/server/src/services/ticket.service.ts
@@ -3,15 +3,16 @@ class TicketService {
   constructor() {}
 
   getUserTicketsById(userId: number) {
-    return tickets.filter(ticket => ticket.ownerId === userId);
+    return tickets.filter(ticket => ticket.ownerId == userId);
   }
 
   getTicketById(ticketId: number) {
-    return tickets.find(ticket => ticket.id === ticketId);
+    return tickets.find(ticket => ticket.id == ticketId);
   }
 
   updateTicketOwner(ticketId: number, newOwnerId: number) {
-    let ticket = tickets.find(ticket => ticket.id === ticketId);
+    let ticket = tickets.find(ticket => ticket.id == ticketId);
+    console.log(ticket);
     ticket.ownerId = newOwnerId;
     return ticket;
   }
