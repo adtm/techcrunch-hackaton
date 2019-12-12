f6b4b166dd8f623d3e62e1e365498f5595eff767
pkey <kutka.paulius@gmail.com>
2019-12-12 01:11:51 +0100

    Added more data

--- packages/server/src/data.ts
+++ packages/server/src/data.ts
@@ -4,36 +4,38 @@ export const events = [
   {
     id: 1,
     name: "Taylor Swift World Tour",
-    imageSrc: "https://picsum.photos/id/237/200/300"
+    imageSrc: "https://picsum.photos/id/117/200/300"
   },
   {
     id: 2,
     name: "Berlin Rave Party",
-    imageSrc: "https://picsum.photos/id/237/200/300"
+    imageSrc: "https://picsum.photos/id/117/200/300"
   },
   {
     id: 3,
     name: "Pepa Pig - the awakening",
-    imageSrc: "https://picsum.photos/id/237/200/300"
+    imageSrc: "https://picsum.photos/id/117/200/300"
   },
   {
     id: 4,
     name: "TechCrunch Berlin",
-    imageSrc: "https://picsum.photos/id/237/200/300"
+    imageSrc: "https://picsum.photos/id/117/200/300"
   },
   {
     id: 5,
     name: "Some nice concert",
-    imageSrc: "https://picsum.photos/id/237/200/300"
+    imageSrc: "https://picsum.photos/id/117/200/300"
   },
   {
     id: 6,
     name: "Another nice concert",
-    imageSrc: "https://picsum.photos/id/237/200/300"
+    imageSrc: "https://picsum.photos/id/117/200/300"
   }
 ];
 export const tickets: Ticket[] = [
-  { id: 1, ownerId: 1, event: events[0], price: 20 }
+  { id: 1, ownerId: 1, event: events[0], price: 20 },
+  { id: 2, ownerId: 1, event: events[1], price: 30 },
+  { id: 3, ownerId: 1, event: events[2], price: 30 }
 ];
 export const users = [
   { id: 1, name: "John Cena", username: "user2" },
