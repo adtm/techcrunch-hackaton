6353a8379b2866bdc8606b8742784a120f6a66ae
Tomas Eglinskas <tomase@wix.com>
2019-12-12 10:56:36 +0100

    Change links to prod

--- packages/client/src/pages/tickets/index.tsx
+++ packages/client/src/pages/tickets/index.tsx
@@ -57,7 +57,7 @@ class ListSearchProjects extends Component<ListSearchProjectsProps> {
 
   updateOwner = async () => {
     try {
-      await Axios.put('http://localhost:1001/ticket', {
+      await Axios.put('http://ticket-chain.herokuapp.com/ticket', {
         newOwnerId: this.state.ownerId,
         ticketId: this.state.clickedPostId,
       });
--- packages/client/src/services/user.ts
+++ packages/client/src/services/user.ts
@@ -6,9 +6,11 @@ export async function query(): Promise<any> {
 }
 
 export async function queryCurrent(): Promise<any> {
-  const userId = JSON.parse(localStorage.getItem('userId'))
-  const response = await axios.get(`http://localhost:1001/user?userId=${userId.userId}`);
-  return response.data
+  const userId = JSON.parse(localStorage.getItem('userId'));
+  const response = await axios.get(
+    `http://ticket-chain.herokuapp.com/user?userId=${userId.userId}`,
+  );
+  return response.data;
 }
 
 export async function queryNotices(): Promise<any> {
